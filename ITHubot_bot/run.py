32a392ec97b9d473dec34ef142f89a85bb184964
stormxanbo1 <stormxanbo1@gmail.com>
2024-06-18 20:16:41 +0300

    ошибка какая-то с ReplyKeyboardMarkup
    ошибка какая-то с ReplyKeyboardMarkup

--- ITHubot_bot/run.py
+++ ITHubot_bot/run.py
@@ -3,21 +3,40 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('Тесты').add('Рейтинг').add('Мои результаты').add('Помощь')
+
+
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('command /start')
+    await message.answer(
+        "Привет! Я бот для прохождения тестов. Вы можете выбрать тест из списка и начать его прохождение.\n"
+        "\n"
+        "Используйте команду /quiz для выбора теста.\n"
+        "\n"
+        "Для получения помощи используйте команду /help.",
+        reply_markup=main
+    )
 
 
 @dp.message(Command('help'))
 async def get_help(message: Message):
-    await message.answer('command /help')
+    await message.answer(
+        "Этот бот предназначен для прохождения тестов. Вот список доступных команд:\n"
+        "/start - начать прохождение тестов или получить список команд\n"
+        "/quiz - выбрать и начать прохождение теста\n"
+        "/rating - посмотреть рейтинг участников\n"
+        "/results - посмотреть свои результаты\n"
+        "\n"
+        "Если у вас возникли вопросы или проблемы, обратитесь к администратору."
+    )
 
 
 async def main():
