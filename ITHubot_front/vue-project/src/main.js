6fa757b6e2583b75ec9d7372e61608e2829bc65c
Spinter1337 <115459831+Spinter1337@users.noreply.github.com>
2024-06-19 00:43:27 +0300

    /front

---
+++ ITHubot_front/vue-project/src/main.js
@@ -0,0 +1,13 @@
+// import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueCookies from 'vue-cookies';
+
+
+
+const app = createApp(App);
+
+app.use(VueCookies);
+app.use(router);
+app.mount('#app');
