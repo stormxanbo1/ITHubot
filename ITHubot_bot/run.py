89e1289ebd1d8598765b11202bf1e81a58aa9e1e
stormxanbo1 <stormxanbo1@gmail.com>
2024-06-20 13:55:50 +0300

    не выводит вопросы и ответы

--- ITHubot_bot/run.py
+++ ITHubot_bot/run.py
@@ -3,13 +3,13 @@ import logging
 import os
 from aiohttp import ClientSession
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, \
     CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
 
 # Загрузить переменные среды из файла .env
 load_dotenv()
@@ -17,7 +17,7 @@ load_dotenv()
 # Инициализируйте бота с помощью токена из переменных среды.
 bot = Bot(token=os.getenv('TOKEN'))
 
-# Инициализируйте диспетчер с FSM и памятью.
+# Инициализируйте диспетчер.
 dp = Dispatcher(storage=MemoryStorage())
 
 # Определить кнопки для главного меню бота
@@ -38,11 +38,9 @@ main_keyboard = ReplyKeyboardMarkup(
 )
 
 
-# Определить состояние FSM
 class TestStates(StatesGroup):
-    SELECTING_TEST = State()
     IN_TEST = State()
-    SHOW_RESULT = State()
+    IN_QUESTION = State()
 
 
 # Определите обработчик для команды /start.
@@ -76,7 +74,7 @@ async def get_help(message: Message):
 
 # Определите обработчик для кнопки "Тесты".
 @dp.message(lambda message: message.text == 'Тесты')
-async def handle_tests_button(message: Message, state: FSMContext):
+async def handle_tests_button(message: Message):
     async with ClientSession() as session:
         try:
             async with session.get('http://localhost:3333/admin/get/test') as response:
@@ -84,18 +82,15 @@ async def handle_tests_button(message: Message, state: FSMContext):
                     tests = await response.json()
                     logging.info(f"Полученные данные: {tests}")  # Логирование полного JSON-ответа
 
-                    # Создание текста для списка тестов
-                    tests_list = "\n\n".join(
-                        [f"Название: {test['title']}\nОписание: {test['description']}" for test in tests])
-
                     # Создание инлайн-кнопок для каждого теста
                     buttons = [InlineKeyboardButton(text=test['title'], callback_data=f"test_{test['testId']}") for test
                                in tests]
                     inline_kb = InlineKeyboardMarkup(inline_keyboard=[buttons])
 
-                    # Отправка сообщения со списком тестов и инлайн-кнопками
+                    # Форматирование списка тестов
+                    tests_list = "\n\n".join(
+                        [f"Название: {test['title']}\nОписание: {test['description']}" for test in tests])
                     await message.answer(f"Список доступных тестов:\n\n{tests_list}", reply_markup=inline_kb)
-                    await state.set_state(TestStates.SELECTING_TEST)
                 else:
                     await message.answer("Произошла ошибка при загрузке списка тестов")
         except Exception as e:
@@ -103,7 +98,7 @@ async def handle_tests_button(message: Message, state: FSMContext):
             await message.answer("Произошла ошибка при выполнении запроса на сервер")
 
 
-# Обработчик для инлайн-кнопок выбора теста
+# Обработчик для инлайн-кнопок
 @dp.callback_query(lambda callback_query: callback_query.data.startswith("test_"))
 async def handle_test_selection(callback_query: CallbackQuery, state: FSMContext):
     test_id = callback_query.data.split("_")[1]
@@ -116,7 +111,9 @@ async def handle_test_selection(callback_query: CallbackQuery, state: FSMContext
                     selected_test = next((test for test in tests if str(test['testId']) == test_id), None)
 
                     if selected_test:
-                        await state.update_data(test_id=test_id, current_question=0, answers=[],
+                        await state.update_data(test_id=test_id,
+                                                current_question=0,
+                                                answers=[],
                                                 selected_test=selected_test)
 
                         await callback_query.message.answer(
@@ -134,7 +131,6 @@ async def handle_test_selection(callback_query: CallbackQuery, state: FSMContext
     await callback_query.answer()
 
 
-# Функция для получения и отправки вопроса
 async def ask_question(message: Message, state: FSMContext):
     data = await state.get_data()
     test_id = data['test_id']
@@ -148,12 +144,22 @@ async def ask_question(message: Message, state: FSMContext):
                     if current_question < len(questions):
                         question = questions[current_question]
                         await state.update_data(questions=questions)
-                        buttons = [InlineKeyboardButton(text=option, callback_data=f"answer_{option}") for option in
-                                   question['options']]
-                        inline_kb = InlineKeyboardMarkup(inline_keyboard=[buttons])
 
-                        await message.answer(f"Вопрос {current_question + 1}: {question['text']}",
-                                             reply_markup=inline_kb)
+                        # Получение ответов на текущий вопрос
+                        async with session.get(
+                                f'http://localhost:3333/admin/get/question/answer/{question["id"]}') as answer_response:
+                            if answer_response.status == 200:
+                                answers = await answer_response.json()
+                                buttons = [
+                                    InlineKeyboardButton(text=answer['text'],
+                                                         callback_data=f"answer_{answer['id']}")
+                                    for answer in answers]
+                                inline_kb = InlineKeyboardMarkup(inline_keyboard=[buttons])
+
+                                await message.answer(f"Вопрос {current_question + 1}: {question['text']}",
+                                                     reply_markup=inline_kb)
+                            else:
+                                await message.answer("Произошла ошибка при загрузке ответов на вопрос")
                     else:
                         await show_result(message, state)
                 else:
@@ -163,33 +169,16 @@ async def ask_question(message: Message, state: FSMContext):
             await message.answer("Произошла ошибка при выполнении запроса на сервер")
 
 
-# Обработчик для инлайн-кнопок выбора ответа
-@dp.callback_query(lambda callback_query: callback_query.data.startswith("answer_"))
-async def handle_answer_selection(callback_query: CallbackQuery, state: FSMContext):
-    answer = callback_query.data.split("_")[1]
-    data = await state.get_data()
-    answers = data['answers']
-    answers.append(answer)
-    current_question = data['current_question'] + 1
-    await state.update_data(answers=answers, current_question=current_question)
-
-    await callback_query.answer()
-    await ask_question(callback_query.message, state)
-
-
-# Функция для показа результата
 async def show_result(message: Message, state: FSMContext):
     data = await state.get_data()
+    selected_test = data['selected_test']
     answers = data['answers']
-    questions = data['questions']
 
-    correct_answers = sum(1 for i, question in enumerate(questions) if question['correct_answer'] == answers[i])
-    total_questions = len(questions)
-    score = (correct_answers / total_questions) * 100
+    # Пример логики для подсчета результатов
+    result = len(answers)  # Подсчитываем количество ответов, можно доработать по логике
 
-    await message.answer(
-        f"Тест завершен! Ваш результат: {correct_answers} из {total_questions} правильных ответов ({score:.2f}%).")
-    await state.set_state(TestStates.SHOW_RESULT)
+    await message.answer(f"Тест '{selected_test['title']}' завершен!\nВаш результат: {result}")
+    await state.finish()
 
 
 # Основная функция для запуска бота
