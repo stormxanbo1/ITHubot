6dc24176dc93c1fbe9ad89dc85faa802f99fc3d7
Spinter1337 <115459831+Spinter1337@users.noreply.github.com>
2024-06-20 18:30:52 +0300

    /front_com

--- ITHubot_front/vue-project/src/components/Header.vue
+++ ITHubot_front/vue-project/src/components/Header.vue
@@ -5,7 +5,6 @@ import api from '@/api';
 export default {
   data() {
     return {
-      searchQuery: '',
       user: {
         name: '',  
       },
@@ -17,133 +16,72 @@ export default {
       this.$cookies.remove('jwt');
       window.location.href="/log"
     },
-    performSearch() {
-      // Здесь можно добавить логику для выполнения поискового запроса
-      // Например, можно использовать маршрут или метод для выполнения поиска
-      // В этом примере, предположим, что есть маршрут "/search", который отображает результаты поиска
-      this.$router.push({ path: '/search', query: { q: this.searchQuery } });
-    },
     mainMenu() {
       window.location.href = '/main';
     },
-
-    
     tgHref(){
       window.location.href = 'https://t.me/ITHubbbbot';
     },
-
-    liderboard(){
+    leaderboard(){
       window.location.href = '/lead';
+    },
+    async fetchUserData() {
+      const token = this.$cookies.get('jwt'); 
+      const decodedToken = jwt_decode.decode(token);
+      if (decodedToken) {
+        this.userId = decodedToken.id;
+        try {
+          const response = await api.get('/admin/get/user/' + this.userId, {
+            headers: {
+              'Authorization': 'Bearer ' + token
+            }
+          });
+          this.user.name = response.data.username;
+        } catch (error) {
+          console.error('Ошибка при получении данных:', error);
+        }
+      } else {
+        console.log('Неверный токен');
+      }
     }
-    
-    
   },
-//   mounted() {
-
-
-//     const token = this.$cookies.get('jwt'); 
-//     const decodedToken = jwt_decode.decode(token);
-
-//     if (decodedToken) {
-//         this.userId = decodedToken.id;
-         
-//     } else {
-//         console.log('Неверный токен');
-//     }
-
-
-
-//     try {
-//     const response =  api.get('/admin/get/user/'+ this.userId, {
-//       headers: {
-//         'Authorization': 'Bearer ' + this.$cookies.get('jwt') 
-//       }
-//     })
-//       .then(response => {
-//         this.user.name = response.data.username
-
-        
-        
-//       })
-//       .catch(error => {
-//         console.error('Ошибка при получении данных:', error);
-//       });
-      
-//     } catch (error) {
-//     console.error(error);
-//   }
-// }
+  mounted() {
+    this.fetchUserData();
+  }
 };
 </script>
 
 <template>
   <header>
-    
     <div class="logo">
       <img
-        src="https://cdn-icons-png.flaticon.com/512/2750/2750276.png"
-        class="logo"
-        alt=""
+        src="https://png.pngtree.com/png-clipart/20230824/original/pngtree-chat-bot-icon-vector-picture-image_8418981.png"
+        class="logo-img"
+        alt="New Logo"
         @click="mainMenu()"
       />
     </div>
     <div class="buttons">
       <button @click="mainMenu()">Список тестов</button>
-      <button @click="liderboard()">Список Лидеров</button>
+      <button @click="leaderboard()">Список Лидеров</button>
       <button @click="tgHref()">Телеграмм Бот</button>
-      
-      
-     
     </div>
-    
-    <div class="UserName">
-      <a> {{ this.user.name }}</a>
+    <div class="user-info">
+      <span>{{ user.name }}</span>
       <button class="logout" @click="logOut()">Выйти из аккаунта</button>
-
     </div>
-    
-    <!-- <div class="search-bar">
-      <input type="text" placeholder="Поиск..." v-model="searchQuery" @keyup.enter="performSearch" />
-      <button @click="performSearch">Найти</button>
-    </div> -->
-    
   </header>
-
-  <main></main>
 </template>
 
 <style scoped>
-.UserName{
-  margin-left: 30%;
-  color: black;
-}
-/* .logo{
-    width: 15%;
-  }
-  .logodiv{
-    display: flex;
-    justify-content: end;
-  }
-  .cad{
-    
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-  } */
-
 header {
-  min-width: 60%;
+  min-width: 90%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 10px 20px;
-  background: rgb(255, 255, 255);
+  background: linear-gradient(241deg, rgb(255, 255, 255), rgb(253, 237, 255));
   border-radius: 30px;
-  background: linear-gradient(
-    241deg,
-    rgb(255, 255, 255) 0%,
-    rgb(253, 237, 255) 100%
-  );
   animation: gradient 9s infinite linear;
   background-size: 400%;
   color: white;
@@ -151,43 +89,54 @@ header {
 
 .logo {
   flex: 1;
-  max-width: 50px;
-  margin-right: 5%;
 }
 
-.search-bar {
-  flex: 2;
-}
-
-.search-bar input {
-  padding: 5px;
-  width: 200px;
-}
-
-.search-bar button {
-  padding: 5px 10px;
-  background-color: #45184c;
-  color: white;
-  border: none;
+.logo-img {
+  max-width: 100px;
   cursor: pointer;
 }
 
 .buttons {
   flex: 1;
   display: flex;
+  justify-content: flex-end;
 }
 
 .buttons button {
-  
-
-  padding: 5px 10px;
   margin-left: 10px;
+  padding: 8px 16px;
   background: none;
-  color: rgb(15, 15, 15);
+  color: #0f0f0f;
   border: none;
   border-radius: 3px;
   cursor: pointer;
 }
+
+.user-info {
+  flex: 1;
+  display: flex;
+  justify-content: flex-end; 
+  align-items: center;
+}
+
+.user-info span {
+  margin-right: 10px;
+  color: black;
+}
+
+.logout {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.logout:hover {
+  background-color: #0056b3;
+}
+
 @keyframes gradient {
   0% {
     background-position: 80% 0%;
@@ -201,28 +150,14 @@ header {
 }
 
 @media (max-width: 768px) {
-  .search-bar input {
-    width: 150px;
-  }
-}
-
-@media (max-width: 480px) {
   header {
     flex-direction: column;
     padding: 10px;
   }
-  .search-bar {
-    width: 100%;
-    align-items: center;
-    display: contents;
-  }
-  .search-bar input {
-    width: 100%;
-    margin-bottom: 5px;
-  }
 
   .buttons {
-    align-items: end;
+    flex-direction: column;
+    align-items: center;
     margin-top: 10px;
   }
 
@@ -230,6 +165,16 @@ header {
     margin-left: 0;
     display: block;
     width: 100%;
+    margin-top: 7px;
+  }
+
+  .user-info {
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10px;
+  }
+
+  .user-info button {
     margin-top: 7px;
   }
 }
