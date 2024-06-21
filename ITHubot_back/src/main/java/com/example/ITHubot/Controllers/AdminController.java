47ca79a5ea13d72d51b42efa8a08c7f7154c3e88
Spinter1337 <115459831+Spinter1337@users.noreply.github.com>
2024-06-21 13:12:20 +0300

    /ADminka

--- ITHubot_back/src/main/java/com/example/ITHubot/Controllers/AdminController.java
+++ ITHubot_back/src/main/java/com/example/ITHubot/Controllers/AdminController.java
@@ -24,39 +24,33 @@ import java.util.Set;
 public class AdminController {
     private final DataAccessLayer dataAccessLayer;
     private final UserDetailsServiceImpl userService;
-    /*   private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
-       public void someMethod() {
-           logger.debug("Debug message");
-   //        logger.info("Info message");
-           logger.warn("Warning message");
-           logger.error("Error message");
-       }*/
+    private final JwtCore jwtCore;
+    private final PasswordEncoder passwordEncoder;
+
+    private final TestEvaluationService testEvaluationService;
     @Autowired
-    public AdminController(UserDetailsServiceImpl userService, DataAccessLayer dataAccessLayer) {
+    public AdminController(UserDetailsServiceImpl userService, DataAccessLayer dataAccessLayer, JwtCore jwtCore, PasswordEncoder passwordEncoder, TestEvaluationService testEvaluationService) {
         this.userService = userService;
         this.dataAccessLayer = dataAccessLayer;
+        this.jwtCore = jwtCore;
+        this.passwordEncoder = passwordEncoder;
+        this.testEvaluationService = testEvaluationService;
     }
-    @Autowired
-    private JwtCore jwtCore;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private TestEvaluationService testEvaluationService;
-    @PostMapping("/create")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<?> createAdmin(@RequestBody SignupRequest signupRequest) {
-//        signupRequest.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
-        signupRequest.setRoles(Set.of("ROLE_ADMIN"));
-        String serviceResult = userService.newUser(signupRequest);
-        if (Objects.equals(serviceResult, "Выберите другое имя")) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(serviceResult);
-        }
-        if (Objects.equals(serviceResult, "Выберите другую почту")) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(serviceResult);
-        }
-        return ResponseEntity.ok("Администратор успешно создан.");
-    }
+//    @PostMapping("/create")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    public ResponseEntity<?> createAdmin(@RequestBody SignupRequest signupRequest) {
+////        signupRequest.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+//        signupRequest.setRoles(Set.of("ROLE_ADMIN"));
+//        String serviceResult = userService.newUser(signupRequest);
+//        if (Objects.equals(serviceResult, "Выберите другое имя")) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(serviceResult);
+//        }
+//        if (Objects.equals(serviceResult, "Выберите другую почту")) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(serviceResult);
+//        }
+//        return ResponseEntity.ok("Администратор успешно создан.");
+//    }
 
 
 //    @GetMapping("/get/users")
@@ -65,154 +59,184 @@ public class AdminController {
 //    }
 
     @PostMapping("/create/user")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity createUser(@RequestBody User user){
         dataAccessLayer.createUser(user);
         return ResponseEntity.ok("User added successfully!");
     }
     @DeleteMapping("/delete/user/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity deleteUserById(@PathVariable("id") long id){
         dataAccessLayer.deleteUserById(id);
         return ResponseEntity.ok("User deleted successfully!");
     }
     @PostMapping("update/user/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity updateUserById(@PathVariable("id") long id, @RequestBody User newUser) {
         dataAccessLayer.updateUser(id, newUser);
         return ResponseEntity.ok("User updated!");
     }
     @GetMapping("get/user/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getUserById(@PathVariable("id") long id) {
 
         return ResponseEntity.ok((dataAccessLayer.getUserById(id)));
     }
 
     @GetMapping("/get/answer")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getAnswer(){
         return ResponseEntity.ok(dataAccessLayer.getAnswer());
     }
 
     @PostMapping("/create/answer")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity createAnswer(@RequestBody Answer answer){
         dataAccessLayer.createAnswer(answer);
         return ResponseEntity.ok("User added successfully!");
     }
     @DeleteMapping("/delete/answer/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity deleteAnswerById(@PathVariable("id") long id){
         dataAccessLayer.deleteAnswerById(id);
         return ResponseEntity.ok("User deleted successfully!");
     }
     @PostMapping("update/answer/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity updateAnswerById(@PathVariable("id") long id, @RequestBody Answer newAnswer) {
         dataAccessLayer.updateAnswer(id, newAnswer);
         return ResponseEntity.ok("User updated!");
     }
     @GetMapping("/get/question")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getQuestion(){
         return ResponseEntity.ok(dataAccessLayer.getQuestion());
     }
 
     @PostMapping("/create/question")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity createQuestion(@RequestBody Question question){
         dataAccessLayer.createQuestion(question);
         return ResponseEntity.ok("User added successfully!");
     }
     @DeleteMapping("/delete/question/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity deleteQuestionById(@PathVariable("id") long id){
         dataAccessLayer.deleteQuestionById(id);
         return ResponseEntity.ok("User deleted successfully!");
     }
     @PostMapping("update/question/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity updateQuestionById(@PathVariable("id") long id, @RequestBody Question newQuestion) {
         dataAccessLayer.updateQuestion(id, newQuestion);
         return ResponseEntity.ok("User updated!");
     }
     @GetMapping("/get/result")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getResult(){
         return ResponseEntity.ok(dataAccessLayer.getAnswer());
     }
 
     @PostMapping("/create/result")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity createResult(@RequestBody Result result){
         dataAccessLayer.createResult(result);
         return ResponseEntity.ok("Result added successfully!");
     }
     @DeleteMapping("/delete/result/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity deleteResultById(@PathVariable("id") long id){
         dataAccessLayer.deleteResultById(id);
         return ResponseEntity.ok("User deleted successfully!");
     }
     @PostMapping("update/result/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity updateResultById(@PathVariable("id") long id, @RequestBody Result newResult) {
         dataAccessLayer.updateResult(id, newResult);
         return ResponseEntity.ok("User updated!");
     }
     @GetMapping("/get/test")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getTest(){
         return ResponseEntity.ok(dataAccessLayer.getTest());
     }
 
     @PostMapping("/create/test")
-    public ResponseEntity createTest(@RequestBody Test Test){
-        dataAccessLayer.createTest(Test);
-        return ResponseEntity.ok("User added successfully!");
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity createTest(@RequestBody Test test){
+        dataAccessLayer.createTest(test);
+        return ResponseEntity.ok("Test added successfully!");
     }
     @DeleteMapping("/delete/test/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity deleteTestById(@PathVariable("id") long id){
         dataAccessLayer.deleteTestById(id);
         return ResponseEntity.ok("User deleted successfully!");
     }
     @PostMapping("update/test/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity updateTestById(@PathVariable("id") long id, @RequestBody Test newTest) {
         dataAccessLayer.updateTest(id, newTest);
         return ResponseEntity.ok("User updated!");
     }
     @GetMapping("/get/score")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getUserScore(){
         return ResponseEntity.ok(dataAccessLayer.getAnswer());
     }
 
     @PostMapping("/create/userscore")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity createUserScore(@RequestBody UserScore userScore ){
         dataAccessLayer.createUserScore(userScore);
         return ResponseEntity.ok("User added successfully!");
     }
     @DeleteMapping("/delete/userscore/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity deleteUserScoreById(@PathVariable("id") long id){
         dataAccessLayer.deleteUserScoreById(id);
         return ResponseEntity.ok("User deleted successfully!");
     }
     @PostMapping("update/userscore/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity updateUserScoreById(@PathVariable("id") long id, @RequestBody UserScore newUserScore) {
         dataAccessLayer.updateUserScore(id, newUserScore);
         return ResponseEntity.ok("User updated!");
     }
 
     @GetMapping("update/result/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getResultById(@PathVariable("id") long id) {
         dataAccessLayer.getResultById(id);
         return ResponseEntity.ok("result updated!");
     }
     @GetMapping("/get/results")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getResults(){
         return ResponseEntity.ok(dataAccessLayer.getResults());
     }
 
     @GetMapping("get/test/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getTestById(@PathVariable("id") long id) {
 
         return ResponseEntity.ok(dataAccessLayer.getTestById(id));
     }
     @GetMapping("update/userScore/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getUserScoreById(@PathVariable("id") long id) {
         dataAccessLayer.getUserScoreById(id);
         return ResponseEntity.ok("result updated!");
     }
 
     @GetMapping("/get/userScores")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getUserScores(){
         return ResponseEntity.ok(dataAccessLayer.getUserScores());
     }
 
     @GetMapping("get/question/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getQuestionById(@PathVariable("id") long id) {
         dataAccessLayer.getQuestionById(id);
         return ResponseEntity.ok("User updated!");
@@ -220,6 +244,7 @@ public class AdminController {
 
 
     @GetMapping("get/answer/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity getAnswerById(@PathVariable("id") long id) {
 
         return ResponseEntity.ok(dataAccessLayer.getAnswerById(id));
@@ -227,6 +252,7 @@ public class AdminController {
     /////////////////////////////////////////////////////////
 
     @GetMapping("/questions/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> getQuestionsByTestId(@PathVariable("id") Long id) {
         List<Question> questions = dataAccessLayer.getQuestionsByTestID(id);
         if (questions.isEmpty()) {
@@ -235,6 +261,7 @@ public class AdminController {
         return ResponseEntity.ok(questions);
     }
     @GetMapping("get/question/answer/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> getAnswerByQuestionId(@PathVariable("id") Long id) {
         List<Answer> answers = dataAccessLayer.getAnswersByQuestionId(id);
         if (answers.isEmpty()) {
@@ -244,6 +271,7 @@ public class AdminController {
     }
 
     @GetMapping("/score/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<UserScore> getUserScoreByUserId(@PathVariable("id") Long id) {
         UserScore userScore = dataAccessLayer.getUserScoreByUserId(id);
         if (userScore == null) {
