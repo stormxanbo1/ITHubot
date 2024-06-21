efaf91d7dbdff67a0eee800695a3030d684f513d
Spinter1337 <115459831+Spinter1337@users.noreply.github.com>
2024-06-21 13:26:01 +0300

    /admin

--- ITHubot_back/src/main/java/com/example/ITHubot/Controllers/MainController.java
+++ ITHubot_back/src/main/java/com/example/ITHubot/Controllers/MainController.java
@@ -20,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 @RestController
@@ -78,6 +79,87 @@ public ResponseEntity<?> createResult(@RequestBody ResultRequest resultRequest) 
 
     return ResponseEntity.ok("Result created successfully");
 }
+    @GetMapping("/get/userScores")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity getUserScores(){
+        return ResponseEntity.ok(dataAccessLayer.getUserScores());
+    }
+
+    @GetMapping("get/question/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity getQuestionById(@PathVariable("id") long id) {
+        dataAccessLayer.getQuestionById(id);
+        return ResponseEntity.ok("User updated!");
+    }
+
+
+    @GetMapping("get/answer/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity getAnswerById(@PathVariable("id") long id) {
+
+        return ResponseEntity.ok(dataAccessLayer.getAnswerById(id));
+    }
+    /////////////////////////////////////////////////////////
+
+    @GetMapping("/questions/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> getQuestionsByTestId(@PathVariable("id") Long id) {
+        List<Question> questions = dataAccessLayer.getQuestionsByTestID(id);
+        if (questions.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No questions found for the given test");
+        }
+        return ResponseEntity.ok(questions);
+    }
+    @GetMapping("get/question/answer/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> getAnswerByQuestionId(@PathVariable("id") Long id) {
+        List<Answer> answers = dataAccessLayer.getAnswersByQuestionId(id);
+        if (answers.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No questions found for the given test");
+        }
+        return ResponseEntity.ok(answers);
+    }
+
+    @GetMapping("/score/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<UserScore> getUserScoreByUserId(@PathVariable("id") Long id) {
+        UserScore userScore = dataAccessLayer.getUserScoreByUserId(id);
+        if (userScore == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(userScore, HttpStatus.OK);
+    }
+    @GetMapping("get/user/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity getUserById(@PathVariable("id") long id) {
+
+        return ResponseEntity.ok((dataAccessLayer.getUserById(id)));
+    }
+
+    @GetMapping("/get/answer")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity getAnswer(){
+        return ResponseEntity.ok(dataAccessLayer.getAnswer());
+    }
+
+
+    @GetMapping("/get/test")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity getTest(){
+        return ResponseEntity.ok(dataAccessLayer.getTest());
+    }
+    @GetMapping("/get/results")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity getResults(){
+        return ResponseEntity.ok(dataAccessLayer.getResults());
+    }
+
+    @GetMapping("get/test/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity getTestById(@PathVariable("id") long id) {
+
+        return ResponseEntity.ok(dataAccessLayer.getTestById(id));
+    }
 
 
 }
